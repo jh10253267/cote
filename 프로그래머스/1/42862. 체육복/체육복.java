4b94eb629a254e0848b0b36aaa62771f394984f4
Gwon Junhyeok <108499717+jh10253267@users.noreply.github.com>
2024-01-01 22:01:32 +0900

    [level 1] Title: 체육복, Time: 0.25 ms, Memory: 74.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/42862. 체육복/체육복.java
@@ -0,0 +1,42 @@
+//배열을 순회한다
+//
+class Solution {
+    public int solution(int n, int[] lost, int[] reserve) {
+        int answer = 0;
+        //n개의 배열 생성
+        int[] arr = new int[n];
+        int length = arr.length;
+        for(int i = 0; i<length; i++) {
+            arr[i] = 1;
+        }
+        for(int i : reserve) {
+            arr[i-1] += 1;
+        }
+        for(int i : lost) {
+            arr[i-1] -= 1;
+        }
+        
+        for(int i=0; i<length; i++ ) {
+            if(arr[i] > 1) {
+                if(i == 0 && arr[i+1] == 0) {
+                    arr[i+1] += 1;
+                    arr[i] -= 1;
+                } else if(i>0 && arr[i-1] == 0) {
+                    arr[i-1] += 1;
+                    arr[i] -= 1;
+                } else if(i < length-1 && arr[i+1] == 0) {
+                    arr[i+1] += 1;
+                    arr[i] -= 1;
+                }
+            }
+        }
+        for(int i : arr) {
+            System.out.println(i);
+            if(i > 0) {
+                answer ++;
+            }
+        }
+        
+        return answer;
+    }
+}
