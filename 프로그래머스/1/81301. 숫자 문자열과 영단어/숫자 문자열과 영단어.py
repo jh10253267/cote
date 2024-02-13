56f8de1fd7ff23f76423f077ede5e853fdaac70b
Gwon Junhyeok <108499717+jh10253267@users.noreply.github.com>
2024-02-13 14:55:34 +0900

    [level 1] Title: 숫자 문자열과 영단어, Time: 0.02 ms, Memory: 10.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.py
@@ -0,0 +1,23 @@
+# 1. 문제 이해하기
+# 2. 데이터 가공하기
+# 3. 하드코딩
+# 4. 일반화하기
+
+def solution(s):
+    eng_num = {
+        "zero" : "0",
+        "one" : "1",
+        "two" : "2",
+        "three" : "3",
+        "four" : "4",
+        "five" : "5",
+        "six" : "6",
+        "seven" : "7",
+        "eight" : "8",
+        "nine" : "9"
+    }
+    
+    for eng, num in eng_num.items() :
+        if eng in s:
+            s = s.replace(eng, num)
+    return int(s)
