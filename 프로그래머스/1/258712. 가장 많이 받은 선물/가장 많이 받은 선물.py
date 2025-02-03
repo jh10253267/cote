9841b0a7bbc2237f3e2598bf3343fb23dece7e15
Gwon Junhyeok <108499717+jh10253267@users.noreply.github.com>
2025-02-03 23:01:52 +0900

    [level 1] Title: 가장 많이 받은 선물, Time: 11.87 ms, Memory: 10.7 MB -BaekjoonHub

--- 프로그래머스/1/258712. 가장 많이 받은 선물/가장 많이 받은 선물.py
+++ 프로그래머스/1/258712. 가장 많이 받은 선물/가장 많이 받은 선물.py
@@ -1,37 +1,39 @@
-from collections import defaultdict
-
 def solution(friends, gifts):
     n = len(friends)
-    give = [defaultdict(int) for _ in range(n)]
-    take = [0] * n
-    gift_index = {friend: i for i, friend in enumerate(friends)}
-    gift_score = [0] * n
-
+    g_arr = [[0] * len(friends) for _ in range(n)]
+    t_arr = [0] * n
+    g_score = [0] * n
+    
     for gift in gifts:
         giver, taker = gift.split()
-        g_idx = gift_index[giver]
-        t_idx = gift_index[taker]
-
-        give[g_idx][t_idx] += 1
-        take[t_idx] += 1
-
-    for i in range(n):
-        gift_score[i] = sum(give[i].values()) - take[i]
-
-    max_gift = 0
-    for i in range(n):
-        possible_gifts = 0
+        g_idx = friends.index(giver)
+        t_idx = friends.index(taker)
         
+        g_arr[g_idx][t_idx] += 1
+        t_arr[t_idx] += 1
+    
+    for i in range(n):
+        g_score[i] = sum(g_arr[i]) - t_arr[i]
+    
+    max_gift = [0] * n
+    
+    for i in range(n):
         for j in range(n):
-            if i == j:
+            if i==j:
                 continue
+                
+            g_cout = g_arr[i][j]
+            t_cout = g_arr[j][i]
             
-            if give[i][j] > give[j][i]:
-                possible_gifts += 1
-            elif give[i][j] == give[j][i]:
-                if gift_score[i] > gift_score[j]:
-                    possible_gifts += 1
-
-        max_gift = max(max_gift, possible_gifts)
-
-    return max_gift
+            if g_cout > t_cout:
+                max_gift[i] += 1
+            # 서로 주고받았지만 동수인 경우
+            elif (g_cout > 0 and t_cout > 0) and (g_cout==t_cout):
+                if g_score[j] < g_score[i]:
+                    max_gift[i] += 1
+            # 서로 주고받은 적이 없다.
+            elif (g_cout == 0 and t_cout == 0) and (g_score[j] < g_score[i]):
+                max_gift[i] += 1
+            
+                
+    return max(max_gift)
