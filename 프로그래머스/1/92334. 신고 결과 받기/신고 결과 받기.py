0609f648d207403c52d2a13920a11099757a06d0
Gwon Junhyeok <108499717+jh10253267@users.noreply.github.com>
2025-02-16 06:55:13 +0900

    [level 1] Title: 신고 결과 받기, Time: 1284.10 ms, Memory: 39.6 MB -BaekjoonHub

--- 프로그래머스/1/92334. 신고 결과 받기/신고 결과 받기.py
+++ 프로그래머스/1/92334. 신고 결과 받기/신고 결과 받기.py
@@ -1,17 +1,14 @@
-from collections import defaultdict
-
 def solution(id_list, report, k):
-    report_set = set(report)
-    reported_counts = defaultdict(int)
-    notifications = defaultdict(int)
+    answer = [0] * len(id_list)
+    reported_dict = {x:0 for x in id_list}
+    report = set(report)
     
-    for report_entry in report_set:
-        reporter, reported = report_entry.split()
-        reported_counts[reported] += 1
+    for r in report:
+        reported_user = r.split()[1]
+        reported_dict[reported_user] += 1
+    for r in report:
+        report_user, reported_user = r.split()
+        if reported_dict[reported_user] >= k:
+            answer[id_list.index(report_user)] += 1    
     
-    for report_entry in report_set:
-        reporter, reported = report_entry.split()
-        if reported_counts[reported] >= k:
-            notifications[reporter] += 1
-    
-    return [notifications[user_id] for user_id in id_list]
+    return answer
