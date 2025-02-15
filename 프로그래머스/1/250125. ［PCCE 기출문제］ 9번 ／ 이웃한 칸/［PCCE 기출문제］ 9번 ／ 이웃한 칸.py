7c5cda157d2d815d82703367dda25786a6219ef9
Gwon Junhyeok <108499717+jh10253267@users.noreply.github.com>
2025-02-16 06:51:54 +0900

    [level 1] Title: [PCCE 기출문제] 9번 / 이웃한 칸, Time: 0.01 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/1/250125. ［PCCE 기출문제］ 9번 ／ 이웃한 칸/［PCCE 기출문제］ 9번 ／ 이웃한 칸.py
+++ 프로그래머스/1/250125. ［PCCE 기출문제］ 9번 ／ 이웃한 칸/［PCCE 기출문제］ 9번 ／ 이웃한 칸.py
@@ -1,15 +1,14 @@
-# 격자 보드에서 상하좌우중 같은 색이 칠해진 칸의 갯수 구하기
-# w는 x좌표 h는 y좌표
-# 탐색 순서는 오른쪽, 아래, 위, 왼쪽 
 def solution(board, h, w):
     answer = 0
-    dh = [1, -1, 0, 0]
-    dw = [0, 0, 1, -1]
+    dh = [1, -1, 0, 0] # 상하
+    dw = [0, 0, 1, -1] # 좌우
     target = board[h][w]
-    
+
     for i in range(len(dh)):
-        h_check, w_check = h + dh[i], w + dw[i]
-        if 0 <= h_check < len(board) and 0 <= w_check < len(board):
-            if board[h_check][w_check] == target:
-                answer += 1
+      h_check, w_check = h + dh[i], w + dw[i]
+
+      if 0 <= h_check < len(board) and 0 <= w_check < len(board[0]):
+        if board[h_check][w_check] == target:
+          answer += 1
+
     return answer
