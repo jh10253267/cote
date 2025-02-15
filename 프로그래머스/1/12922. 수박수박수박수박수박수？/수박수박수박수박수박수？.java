102609c9c390d216042ed286ddd6628f7307037c
Gwon Junhyeok <108499717+jh10253267@users.noreply.github.com>
2025-02-16 06:42:53 +0900

    [level 1] Title: 수박수박수박수박수박수?, Time: 2.66 ms, Memory: 75.2 MB -BaekjoonHub

---
+++ 프로그래머스/1/12922. 수박수박수박수박수박수？/수박수박수박수박수박수？.java
@@ -0,0 +1,14 @@
+class Solution {
+    public String solution(int n) {
+        String answer = "";
+        String temp = "수박";
+        
+        if(n%2 == 0 ) {
+            answer = temp.repeat(n/2);
+        } else {
+            answer = temp.repeat(n/2) + "수";
+        }
+        
+        return answer;
+    }
+}
