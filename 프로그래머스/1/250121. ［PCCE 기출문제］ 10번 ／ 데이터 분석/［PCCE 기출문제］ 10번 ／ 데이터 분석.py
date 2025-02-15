b30220f5e9d52fb334a3061ebd807a64c92f9e0d
Gwon Junhyeok <108499717+jh10253267@users.noreply.github.com>
2025-02-16 06:53:39 +0900

    [level 1] Title: [PCCE 기출문제] 10번 / 데이터 분석, Time: 0.23 ms, Memory: 10.5 MB -BaekjoonHub

--- 프로그래머스/1/250121. ［PCCE 기출문제］ 10번 ／ 데이터 분석/［PCCE 기출문제］ 10번 ／ 데이터 분석.py
+++ 프로그래머스/1/250121. ［PCCE 기출문제］ 10번 ／ 데이터 분석/［PCCE 기출문제］ 10번 ／ 데이터 분석.py
@@ -1,11 +1,11 @@
 def solution(data, ext, val_ext, sort_by):
+    dict = {"code":0, "date":1, "maximum":2, "remain":3}
     answer = []
-    dict = {'code': 0, 'date': 1, 'maximum': 2, 'remain': 3}
-
+    
     for i in data:
         if i[dict[ext]] < val_ext:
             answer.append(i)
-
-    answer.sort(key = lambda x: x[dict[sort_by]])
+            
+    answer.sort(key = lambda x : x[dict[sort_by]])
     
     return answer
