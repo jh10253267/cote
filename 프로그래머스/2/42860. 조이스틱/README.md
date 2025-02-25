3782fdfc7799e63038782ad7ece14b4d751b36ac
Gwon Junhyeok <108499717+jh10253267@users.noreply.github.com>
2025-02-25 23:02:35 +0900

    [level 2] Title: 조이스틱, Time: 0.02 ms, Memory: 10 MB -BaekjoonHub

--- 프로그래머스/2/42860. 조이스틱/README.md
+++ 프로그래머스/2/42860. 조이스틱/README.md
@@ -1,10 +1,10 @@
 # [level 2] 조이스틱 - 42860 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42860#) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42860) 
 
 ### 성능 요약
 
-메모리: 10.2 MB, 시간: 0.03 ms
+메모리: 10 MB, 시간: 0.02 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 02월 23일 01:55:36
+2025년 02월 25일 23:02:28
 
 ### 문제 설명
 
--- 프로그래머스/2/42860. 조이스틱/조이스틱.py
+++ 프로그래머스/2/42860. 조이스틱/조이스틱.py
@@ -1,28 +1,18 @@
 def solution(name):
-    # 각 알파벳 변경에 필요한 조작 횟수 계산
     answer = 0
-    n = len(name)
+    length = len(name)
+    start = 65
+    end = 90
+    min_val = length-1
     
-    # 좌우 이동 최소값 구하기
-    min_move = n - 1  # 최초 최소 이동횟수는 그냥 오른쪽으로 쭉 가는 경우
-    
-    for i in range(n):
-        # 알파벳 변경 횟수 추가
-        answer += min(ord(name[i]) - ord('A'), ord('Z') - ord(name[i]) + 1)
-        
-        # 해당 알파벳 다음부터 연속된 A 문자열 찾기
-        next = i + 1
-        while next < n and name[next] == 'A':
+    for i, val in enumerate(name):
+        answer += min(end-ord(val)+1, ord(val)-start)
+        next = i+1
+        while next < length and name[next] == 'A':
             next += 1
             
-        # 현재 위치에서의 최소 이동 계산
-        # 1. 왼쪽으로 갔다가 오른쪽
-        # 2. 오른쪽으로 갔다가 왼쪽
-        min_move = min([
-            min_move,
-            2 * i + n - next,  # 왼쪽으로 갔다가 오른쪽으로
-            i + 2 * (n - next)  # 오른쪽으로 갔다가 왼쪽으로
-        ])
-    
-    answer += min_move
+        min_val = min(min_val,
+                     i*2 + length-next,
+                     i + 2*(length-next))
+    answer += min_val
     return answer
