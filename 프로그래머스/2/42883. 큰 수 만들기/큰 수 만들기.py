319256bd7c6ffe83525b9f9eeff2d82941fc45ff
Gwon Junhyeok <108499717+jh10253267@users.noreply.github.com>
2025-03-01 20:16:41 +0900

    [level 2] Title: 큰 수 만들기, Time: 100.01 ms, Memory: 14.7 MB -BaekjoonHub

--- 프로그래머스/2/42883. 큰 수 만들기/큰 수 만들기.py
+++ 프로그래머스/2/42883. 큰 수 만들기/큰 수 만들기.py
@@ -1,11 +1,15 @@
 def solution(number, k):
     answer = ''
     stack = []
+    limit = len(number) - k
+    arr = list(map(int, number))
     
-    for num in number:
+    for num in arr:
         while k > 0 and stack and stack[-1] < num:
             stack.pop()
             k -= 1
+            print("!!")
         stack.append(num)
+        print(stack)
         
-    return "".join(stack[:len(stack) - k])  
+    return "".join(map(str, stack))
