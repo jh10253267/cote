9250eec5e9288e3d0c5ce0b7ce440c9157ff4b1d
Gwon Junhyeok <108499717+jh10253267@users.noreply.github.com>
2025-09-21 19:29:55 +0900

    [level 0] Title: 자릿수 더하기, Time: 0.04 ms, Memory: 4.21 MB -BaekjoonHub

---
+++ 프로그래머스/0/120906. 자릿수 더하기/자릿수 더하기.cpp
@@ -0,0 +1,16 @@
+#include <string>
+#include <vector>
+#include <iostream>
+
+using namespace std;
+
+int solution(int n) {
+    int answer = 0;
+    string str = to_string(n);
+
+    for(int i = 0; i<str.length(); i++) {
+        answer += str[i] - '0';
+    }
+    
+    return answer;
+}
