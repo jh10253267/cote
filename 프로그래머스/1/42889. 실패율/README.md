a0e9437c056ed555bc0f2a07d1fa4a6ec2d0e769
Gwon Junhyeok <108499717+jh10253267@users.noreply.github.com>
2025-11-02 07:31:45 +0900

    [level 1] Title: 실패율, Time: 9.59 ms, Memory: 87.4 MB -BaekjoonHub

--- 프로그래머스/1/42889. 실패율/README.md
+++ 프로그래머스/1/42889. 실패율/README.md
@@ -1,10 +1,10 @@
 # [level 1] 실패율 - 42889 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42889) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42889?language=java) 
 
 ### 성능 요약
 
-메모리: 10.2 MB, 시간: 0.01 ms
+메모리: 87.4 MB, 시간: 9.59 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 03월 10일 00:55:32
+2025년 11월 02일 07:31:42
 
 ### 문제 설명
 
---
+++ 프로그래머스/1/42889. 실패율/실패율.java
@@ -0,0 +1,25 @@
+import java.util.HashMap;
+
+class Solution {
+    public int[] solution(int N, int[] stages) {
+        int[] answer = {};
+        int[] challenger = new int[N+2];
+        for(int stage: stages) {
+            challenger[stage] += 1;
+        }
+        
+        HashMap<Integer, Double> fails = new HashMap<>();
+        double total = stages.length;
+        
+        for(int i = 1; i<= N; i++) {
+            if(challenger[i] == 0) {
+                fails.put(i, 0.);
+            } else {
+                fails.put(i, challenger[i] / total);
+                total -= challenger[i];
+            }
+        }
+        
+        return fails.entrySet().stream().sorted((o1, o2) -> Double.compare(o2.getValue(), o1.getValue())).mapToInt(HashMap.Entry::getKey).toArray();
+    }
+}
