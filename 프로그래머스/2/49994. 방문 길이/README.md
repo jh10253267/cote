d85d8b20553324b8ceb7c12fb29b1e9f000fe850
Gwon Junhyeok <108499717+jh10253267@users.noreply.github.com>
2025-11-28 07:38:54 +0900

    [level 2] Title: 방문 길이, Time: 20.34 ms, Memory: 77.8 MB -BaekjoonHub

--- 프로그래머스/2/49994. 방문 길이/README.md
+++ 프로그래머스/2/49994. 방문 길이/README.md
@@ -1,10 +1,10 @@
 # [level 2] 방문 길이 - 49994 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/49994?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/49994) 
 
 ### 성능 요약
 
-메모리: 75.2 MB, 시간: 16.71 ms
+메모리: 77.8 MB, 시간: 20.34 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 11월 27일 07:25:01
+2025년 11월 28일 07:38:53
 
 ### 문제 설명
 
--- 프로그래머스/2/49994. 방문 길이/방문 길이.java
+++ 프로그래머스/2/49994. 방문 길이/방문 길이.java
@@ -2,32 +2,40 @@ import java.util.*;
 
 class Solution {
     private static final HashMap<Character, int[]> location = new HashMap<>();
-    private static boolean isValidMove(int nx, int ny) {
-        return 0 <= nx && nx < 11 && 0 <= ny && ny < 11;
-    }
+ 
     private static void initLocation() {
         location.put('U', new int[]{0, 1});
         location.put('D', new int[]{0, -1});
-        location.put('L', new int[]{1, 0});
-        location.put('R', new int[]{-1, 0});
+        location.put('L', new int[]{-1, 0});
+        location.put('R', new int[]{1, 0});
+    }
+    
+    private static boolean isValidMove(int dx, int dy) {
+        return -5 <= dx && -5 <= dy && dx <= 5 && dy <= 5;
     }
     
     public int solution(String dirs) {
-        initLocation();
-        int x = 5, y = 5;
         HashSet<String> answer = new HashSet<>();
-        for (int i = 0; i< dirs.length(); i++) {
-            int offset[] = location.get(dirs.charAt(i));
-            int nx = x + offset[0];
-            int ny = y + offset[1];
-            if(!isValidMove(nx, ny)) {
+        int x = 0, y = 0;
+        initLocation();
+        
+        for(int i=0; i<dirs.length(); i++) {
+            int[] offset = location.get(dirs.charAt(i));
+            int dx = x + offset[0];
+            int dy = y + offset[1];
+            if(!isValidMove(dx, dy)) {
                 continue;
             }
-            answer.add(x + " " + y + " " + nx + " " + ny);
-            answer.add(nx + " " + ny + " " + x + " " + y);
-            x = nx;
-            y = ny;
+            System.out.println(dx + " " + dy);
+            
+            answer.add(x + " " + y + " " + dx + " " + dy);
+            answer.add(dx + " " + dy + " " + x + " " + y);
+            
+            x = dx;
+            y = dy;
         }
+        
+    
         return answer.size() / 2;
     }
 }
