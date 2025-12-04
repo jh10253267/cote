11db57ba734162d9b7c3471db722f6127a915cc9
Gwon Junhyeok <108499717+jh10253267@users.noreply.github.com>
2025-12-05 02:44:14 +0900

    [level 2] Title: 올바른 괄호, Time: 18.49 ms, Memory: 54.9 MB -BaekjoonHub

--- 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
@@ -2,21 +2,19 @@ import java.util.*;
 
 class Solution {
     boolean solution(String s) {
+        ArrayDeque<Character> stack = new ArrayDeque<>();
         
-        Stack<Character> stack = new Stack<>();
-        for(int i=0; i<s.length(); i++) {
-            if(s.charAt(i) == '(') {
-                stack.push('(');
-            } else if(s.charAt(i) == ')') {
-                if(stack.isEmpty()) {
+        char[] a = s.toCharArray();
+        for(char c : a) {
+            if(c == '(') {
+                stack.push(c);
+            } else {
+                if(stack.isEmpty() || stack.pop() == c) {
                     return false;
                 }
-                stack.pop();
             }
         }
-        if(stack.isEmpty()) {
-            return true;
-        }
-        return false;
+
+        return stack.isEmpty();
     }
 }
